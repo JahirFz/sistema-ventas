51d092ed9594696c4bde4c24b1912ae08625b0fe
JahirFz <jahir.jfz@gmail.com>
2026-04-05 17:42:38 -0600

    uso de with en database

--- Ventas/config/database.py
+++ Ventas/config/database.py
@@ -4,89 +4,74 @@ import os
 RUTA_DB = os.path.join("database", "ventas.db")
 
 def conectar():
-    return sqlite3.connect(RUTA_DB)
+    conn = sqlite3.connect(RUTA_DB)
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
 
 def crear_tabla_clientes():
-    conexion = conectar()
-    cursor = conexion.cursor()
+    with conectar() as conexion:
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS clientes (
-            id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL
-        )
-    """)
-
-    conexion.commit()
-    conexion.close()
+        conexion.execute("""
+            CREATE TABLE IF NOT EXISTS clientes (
+                id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL
+            )
+        """)
 
 def crear_tabla_productos():
-    conexion = conectar()
-    cursor = conexion.cursor()
+    with conectar() as conexion:
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS productos (
-            id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre TEXT NOT NULL,
-            precio REAL NOT NULL
-        )
-    """)
+        conexion.execute("""
+            CREATE TABLE IF NOT EXISTS productos (
+                id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                precio REAL NOT NULL
+            )
+        """)
 
-    conexion.commit()
-    conexion.close()
 
 def crear_tabla_ventas():
-    conexion = conectar()
-    cursor = conexion.cursor()
+    with conectar() as conexion:
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS ventas (
-            id_venta INTEGER PRIMARY KEY AUTOINCREMENT,
-            fecha TEXT NOT NULL,
-            id_cliente INTEGER NOT NULL,
-            requiere_factura INTEGER NOT NULL,
-            total REAL NOT NULL,
-            FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-        )
-    """)
+        conexion.execute("""
+            CREATE TABLE IF NOT EXISTS ventas (
+                id_venta INTEGER PRIMARY KEY AUTOINCREMENT,
+                fecha TEXT NOT NULL,
+                id_cliente INTEGER NOT NULL,
+                requiere_factura INTEGER NOT NULL,
+                total REAL NOT NULL,
+                FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+            )
+        """)
 
-    conexion.commit()
-    conexion.close()
 
 def crear_tabla_detalle_ventas():
-    conexion = conectar()
-    cursor = conexion.cursor()
+    with conectar() as conexion:
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS detalle_ventas (
-            id_detalle INTEGER PRIMARY KEY AUTOINCREMENT,
-            id_venta INTEGER NOT NULL,
-            id_producto INTEGER NOT NULL,
-            cantidad INTEGER NOT NULL,
-            precio_unitario REAL NOT NULL,
-            subtotal REAL NOT NULL,
-            FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
-            FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
-        )
-    """)
+        conexion.execute("""
+            CREATE TABLE IF NOT EXISTS detalle_ventas (
+                id_detalle INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_venta INTEGER NOT NULL,
+                id_producto INTEGER NOT NULL,
+                cantidad INTEGER NOT NULL,
+                precio_unitario REAL NOT NULL,
+                subtotal REAL NOT NULL,
+                FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
+                FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+            )
+        """)
 
-    conexion.commit()
-    conexion.close()
 
 def crear_tabla_pagos():
-    conexion = conectar()
-    cursor = conexion.cursor()
+    with conectar() as conexion:
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS pagos (
-            id_pago INTEGER PRIMARY KEY AUTOINCREMENT,
-            id_venta INTEGER NOT NULL,
-            fecha TEXT NOT NULL,
-            monto REAL NOT NULL,
-            metodo_pago TEXT NOT NULL,
-            FOREIGN KEY (id_venta) REFERENCES ventas(id_venta)
-        )
-    """)
-
-    conexion.commit()
-    conexion.close()
+        conexion.execute("""
+            CREATE TABLE IF NOT EXISTS pagos (
+                id_pago INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_venta INTEGER NOT NULL,
+                fecha TEXT NOT NULL,
+                monto REAL NOT NULL,
+                metodo_pago TEXT NOT NULL,
+                FOREIGN KEY (id_venta) REFERENCES ventas(id_venta)
+            )
+        """)
